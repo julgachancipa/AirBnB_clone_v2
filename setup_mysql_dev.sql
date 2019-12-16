5f3baf9cfbf19e51a1017988ddf84b18e195a02f
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-12-16 21:22:10 +0000

    [feature] - db setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,1 +1,5 @@
--- hola mundo
+-- script to create a db, an user and give priviledges
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_pwd`. * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
