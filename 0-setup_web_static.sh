4add5ee7dc14e9a74c8e69dc0c25b36c0cebf974
julgachancipa <julianaga01@gmail.com>
2020-01-03 20:49:21 +0000

    [Feature] - Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,8 +11,9 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "s/server_name _;/server_name _;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}/" /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
