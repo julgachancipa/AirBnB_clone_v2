a7dd61b298d70a0f75ba789ed9c04b5016d69625
julgachancipa <julianaga01@gmail.com>
2020-01-04 02:50:35 +0000

    [Feature] - comp. files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,8 +10,9 @@ def do_pack():
         local("mkdir -p versions")
         now = datetime.now()
         current_date = now.strftime("%Y%m%d%H%M%S")
-        local("tar -cvzf versions/web_static_{}.tgz web_static"
-              .format(current_date))
-        return out_var
+        f_path = "versions/web_static_" + current_date
+        local("tar -cvzf {}.tgz web_static"
+              .format(f_path))
+        return f_path
     except:
         return None
