cbcad3de2341d328ac2f692cd40be3faaf797f64
julgachancipa <julianaga01@gmail.com>
2020-01-04 06:54:59 +0000

    [Fix] - file_path

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -17,8 +17,8 @@ def do_pack():
         local("mkdir -p versions")
         now = datetime.now()
         current_date = now.strftime("%Y%m%d%H%M%S")
-        f_path = "versions/web_static_" + current_date
-        local("tar -cvzf {}.tgz web_static"
+        f_path = "versions/web_static_" + current_date + ".tgz"
+        local("tar -cvzf {} web_static"
               .format(f_path))
         return f_path
     except:
