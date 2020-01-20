6a9a1e9bcc1f6980034a9dff05f1cec313f8b8e2
julgachancipa <julianaga01@gmail.com>
2020-01-20 21:52:43 +0000

    [FEATURE] - HBNB add new route

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Hello world flask"""
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/number_template/<int:n>')
+def num_template(n):
+    return render_template('5-number.html', n=n)
+
+@app.route('/number/<int:n>')
+def only_int(n):
+    return '%d is a number' % n
+
+@app.route('/python/')
+@app.route('/python/<text>/')
+def py_text(text='is cool'):
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    return 'C %s' % text.replace('_', ' ')
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
