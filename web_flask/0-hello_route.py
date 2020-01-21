7d5486cf20c0e1288d270a63a65120284e278e2d
julgachancipa <julianaga01@gmail.com>
2020-01-21 13:21:34 -0500

    [FIX] - PEP8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,9 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
+    """hello_hbnb"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
